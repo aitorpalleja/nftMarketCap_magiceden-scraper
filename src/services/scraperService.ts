2b1a6aa569e3154f1a17ad2def8a83336762edc0
abengoechea <aitorbengo88@gmail.com>
2022-08-13 11:11:53 +0200

    scrap test

--- src/services/scraperService.ts
+++ src/services/scraperService.ts
@@ -1,31 +1,58 @@
-import { JSDOM } from "jsdom"
-const { window } = new JSDOM('<!doctype html><html><body></body></html>');
+import { JSDOM } from "jsdom";
+const { window } = new JSDOM("<!doctype html><html><body></body></html>");
 const puppeteer: any = require("puppeteer");
 
 export class ScraperService {
-  constructor() {
+  constructor() {}
 
+  async getNftData(symbol: string): Promise<any> {
+    const startTime: number = Date.now() / 1000;
+    const allSymbolsArray: string[] = symbol.split(",");
+    const arrayOfMethods: any = this._getArrayOfScrapMethods(allSymbolsArray, startTime);
+    const [...methodsResult] = (await Promise.all(arrayOfMethods)).filter((a) => a);
+    const result: any[] = [];
+
+    methodsResult.forEach((data: any) => {
+      result.push(data);
+    });
+
+    return result;
   }
 
-  async getNftData(symbol: String): Promise<any> {
-    const browser = await puppeteer.launch({
+  _getArrayOfScrapMethods = (symbols: string[], startTime: number): any => {
+    const arrayOfMethods: any = [];
+    symbols.forEach((symbol) => {
+      arrayOfMethods.push(this._scrapCollectionData(symbol, startTime));
+    });
+
+    return arrayOfMethods;
+  };
+
+  _scrapCollectionData = async (symbol: string, startTime: number) => {
+    let result: any[] = [];
+    const browser: any = await puppeteer.launch({
       headless: true,
-      args: [
-        '--no-sandbox',
-      ]
+      args: ["--no-sandbox"],
     });
     const page: any = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+    await page.setUserAgent(
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+    );
     await page.goto("https://magiceden.io/marketplace/" + symbol);
-
     await page.waitForSelector(".tw-text-white-1.tw-text-14px.tw-truncate");
 
-    const data: any = await page.evaluate(() => {
+    result.push(this._getTimeResult(symbol, startTime));
+    result.push(await this._getPageData(page));
+    await browser.close();
+    return result;
+  };
+
+  private _getPageData = async (page: any) => {
+    const pageData: any = await page.evaluate(() => {
+      const list: any = [];
       let items: any = window.document.getElementsByClassName(
         "tw-text-white-1 tw-text-14px tw-truncate"
       );
-
-      const list: any = [];
 
       for (const item of items) {
         list.push({
@@ -36,8 +63,13 @@ export class ScraperService {
       return list;
     });
 
-    await browser.close();
+    return pageData;
+  };
 
-    return data;
-  }
+  private _getTimeResult = (symbol: string, startTime: number): {} => {
+    return {
+      SYMBOL: symbol,
+      TIME: (Date.now() / 1000 - startTime).toFixed(2) + " segundos",
+    };
+  };
 }
