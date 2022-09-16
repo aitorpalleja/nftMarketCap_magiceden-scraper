304a2aabc384f782173e69f409a75f95182b7184
abengoechea <aitorbengo88@gmail.com>
2022-09-16 19:49:10 +0200

    new job delete all traces every 4 days

--- src/modelsControllers/tracesModelController.ts
+++ src/modelsControllers/tracesModelController.ts
@@ -1,23 +1,29 @@
-import { LogService } from "../services/LogService/LogService";
 import { LogType } from "../services/LogService/LogTypeEnum";
 const tracesModel = require('../models/tracesModel');
 
-exports.saveNewTrace = async (newTrace: any) => {
-    const _logService: LogService = new LogService();
+exports.saveNewTrace = async (newTrace: any, logs) => {
     try {
         await newTrace.save();
     } catch (error) {
-        _logService.log("Error de BBDD, en saveNewTrace. ERROR: " + error, LogType.Error);
+        console.warn("Error de BBDD, en saveNewTrace. ERROR: " + error, LogType.Error);
     }
 }
 
+exports.deleteAllTraces = async () => {
+    try {
+        await tracesModel.deleteMany({});
+    } catch (error) {
+        console.warn("Error de BBDD, en deleteAllTraces. ERROR: " + error, LogType.Error);
+    }
+
+}
+
 exports.getAllTraces = async () => {
-    const _logService: LogService = new LogService();
     let allTraces = null;
     try {
         allTraces = await tracesModel.find();
     } catch (error) {
-        _logService.log("Error de BBDD, en getAllTraces. ERROR: " + error, LogType.Error);
+        console.warn("Error de BBDD, en getAllTraces. ERROR: " + error, LogType.Error);
     }
 
     return allTraces;
