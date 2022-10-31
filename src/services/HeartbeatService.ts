b6c3c0d56006a9631380e0988124ab73c1e03256
abengoechea <aitorbengo88@gmail.com>
2022-10-31 18:54:49 +0100

    correct LogService instance + trycatch

--- src/services/HeartbeatService.ts
+++ src/services/HeartbeatService.ts
@@ -7,13 +7,18 @@ export class HeartBeatService {
     private _hearBeatLog: string = "Hearbeat. Scrapper awake.";
 
     constructor() {
+        this._logService = new LogService();
     }
 
     public startHeartBeat = (): void => {
         setInterval(() => {
-            if (settings.Heartbeat.Enable)
-            {
-                this._log();
+            try {
+                if (settings.Heartbeat.Enable)
+                {
+                    this._log();
+                } 
+            } catch (error) {
+                this._logService.log("Error Hearbeat interval log. Error: " + error, LogType.Error)
             }
         }, settings.Heartbeat.IntervalTime);
     }
